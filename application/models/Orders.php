8ec1f024b31e91ecbc878b261756e06c42d43dbc
ikhangura <ikhangura@gmail.com>
2015-02-11 13:11:32 -0800

    Lab 4 Part a completed // model Signed-off-by:ikhangura <ikhangura@gmail.com>

--- application/models/Orders.php
+++ application/models/Orders.php
@@ -12,49 +12,45 @@ class Orders extends MY_Model {
         parent::__construct('orders', 'num');
     }
 
-    // add an item to an order
+    // add an item to an order 
+    // update database with new order combination
     function add_item($num, $code) {
-        $this->db->from('orderitems');
+    
+       $this->db->from('orderitems');
         $this->db->where('order', $num);
         $this->db->where('item', $code);
-        $query = $this->db->get();
-        $query = $query->result_array();
+        $data = $this->db->get();
+        $data = $data->result_array();
 
-        if(empty($query)){
+        if($data == null){
 
-            $newOrderItem = array( "order" => $num, "item" => $code, "quantity" => 1);
-            $this->db->insert('orderitems', $newOrderItem);
+            $newOrderCombination = array( "order" => $num, "item" => $code, "quantity" => 1);
+            $this->db->insert('orderitems', $newOrderCombination);
         }else{
-            $updateOrder = array ("order" => $num, "item" => $code, "quantity" => $query[0]['quantity'] + 1);
+            $updateOrderTable = array ("order" => $num, "item" => $code, "quantity" => $data[0]['quantity'] + 1);
             $this->db->where('order', $num);
             $this->db->where('item', $code);
-            $this->db->update('orderitems', $updateOrder);
+            $this->db->update('orderitems', $updateOrderTable);
         }
-
-
         $newTotal = array("total" => $this->total($num) );
         $this->db->where('num', $num);
         $this->db->update('orders', $newTotal);
     }
 
     // calculate the total for an order
+    // if you are proceeding with order
     function total($num) {
         $this->db->from('orderitems');
         $this->db->join('menu', 'menu.code = orderitems.item');
         $this->db->where('order', $num);
+        $items = $this->db->get();
+        $items = $items->result_array();
+        $result = 0;
+        foreach($items as $item){
+            $result += $item['quantity'] * $item['price'];
+        } 
 
-        $query = $this->db->get();
-        $query = $query->result_array();
-        //var_dump($query);
-
-        $total = 0;
-        foreach($query as $item){
-            $total += $item['quantity'] * $item['price'];
-        }
-
-        //$this->data['total'] = money_format(LC_MONETARY, $total);
-
-        return $total;
+        return $result;
     }
 
     // retrieve the details for an order
@@ -69,9 +65,7 @@ class Orders extends MY_Model {
             $query = $this->db->get()->result_array();
             $item->name = $query[0]['name'];
         }
-
-        return $orderItems;
-        
+        return $orderItems;        
     }
 
     // cancel an order
@@ -93,32 +87,32 @@ class Orders extends MY_Model {
 
     // validate an order
     // it must have at least one item from each category
+    // return true if  all categories combination true.
     function validate($num) {
+        $boolMeal = false;
+        $boolDrink = false;
+        $boolSweet = false;
         $this->db->from('orderitems');
         $this->db->where('order', $num);
-        $query = $this->db->get();
-        $query = $query->result_array();
-
-        $isMeal = false;
-        $isDrink = false;
-        $isSweet = false;
-        foreach($query as $item){
+        $data = $this->db->get();
+        $data = $data->result_array();
+        
+        foreach($data as $item){
             $this->db->from('menu');
             $this->db->where('code', $item['item']);
             $menuItem = $this->db->get();
             $menuItem = $menuItem->result_array();
-
-
+            
             if($menuItem[0]['category'] == "m"){
-                $isMeal = true;
+                $boolMeal = true;
             }else if($menuItem[0]['category'] == "d"){
-                $isDrink = true;
+                $boolDrink = true;
             }elseif($menuItem[0]['category'] == "s"){
-                $isSweet = true;
+                $boolSweet = true;
             }
         }
 
-        return ($isMeal && $isDrink && $isSweet);
+        return ($boolMeal && $boolDrink && $boolSweet);
     }
 
 }
